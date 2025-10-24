251b4a021451f38e8855248c9b64f60cf45a2e5a
Ivanna Castillo <ikc6@cornell.edu>
2025-10-23 22:45:03 -0400

    New script just to clean the data for spore concentration

---
+++ sporeconcentrationdataclean.R
@@ -0,0 +1,59 @@
+##The broad objective of this script is to clean the data before 
+##graphing and analyzing the 2024 spore concentration experiment. 
+
+#loading the packages
+library(agricolae)
+library(reshape2)
+library(dplyr)
+
+#setting the directory in R
+setwd("/Users/ikc6/Library/CloudStorage/OneDrive-CornellUniversity/Research_PhD/2024 inoculation experiments")
+
+#load the data
+sporeconcentration <- read.csv("SPORECONCDATAFINAL.csv", sep = ";")
+
+#add a column with the disease percentage
+sporeconcentration$disease_percentage <- (
+  (sporeconcentration$yellow + sporeconcentration$dry) / (sporeconcentration$total) *100)
+
+#make it become an integer so that it does not have decimal places
+sporeconcentration$disease_percentage <- as.integer(sporeconcentration$disease_percentage)
+
+#get rid of the columns that we don't need anymore
+sporeconcentration$total <- NULL
+sporeconcentration$wilt <- NULL
+sporeconcentration$yellow <- NULL
+sporeconcentration$dry <- NULL
+sporeconcentration$rep <- NULL
+
+#making each plot a row with a column for each rating so that we can calculate audpc
+sporeconcentration_wide <- reshape(sporeconcentration, 
+                                   idvar = c("plot", "trt"), 
+                                   timevar = "date", 
+                                   direction = "wide",
+                                   v.names = "disease_percentage")
+
+#get rid of the "disease_percentage."
+names(sporeconcentration_wide) <- gsub("disease_percentage.", "", names(sporeconcentration_wide))
+
+#change the format of the column names from %d/%m/%y to %m/%d/%y, using rename, first you assign the new name
+#and then you say which column it is that you want to rename
+sporeconcentration_wide <- sporeconcentration_wide %>%
+  rename("03/29/2024" = "29/03/2024",
+         "04/01/2024" = "01/04/2024",
+         "04/04/2024" = "04/04/2024",
+         "04/09/2024" = "09/04/2024",
+         "04/12/2024" = "12/04/2024",
+         "04/15/2024" = "15/04/2024")
+
+##Calculate dpi
+
+#first make a vector of the dates
+dates <- c("03/29/2024", "04/01/2024", "04/04/2024", "04/09/2024", "04/12/2024", "04/15/2024")
+
+#now subtract the inoculation date ( March 22nd, 2024) from each, to get the number of days post inoculation 
+dpi <- as.Date(dates, format = "%m/%d/%Y") - as.Date("03/22/2024", format = "%m/%d/%Y")
+
+
+
+
