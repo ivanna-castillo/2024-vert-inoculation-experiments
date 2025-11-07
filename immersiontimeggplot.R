c6d04fba88f71f46f83a20da13d3f5c4928e34fd
Ivanna Castillo <ikc6@cornell.edu>
2025-11-07 11:15:18 -0500

    doing the same plot but using ggplot2

---
+++ immersiontimeggplot.R
@@ -0,0 +1,26 @@
+#making the immersion time experiment's plot using ggplot2
+
+#loading the libraries
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+#for using ggplot we need the data in long format
+immersiontime <- read.csv("immersiontime_long_clean.csv")
+
+#creating a column with dpi
+immersiontime$dpi <- as.Date(immersiontime$date, format = "%d/%m/%Y") - as.Date("22/03/2024", format = "%d/%m/%Y")
+
+#making trt a factor
+immersiontime$trt <- as.factor(immersiontime$trt)
+
+#making the plot with ggplot, aes makes the grid and it is where we determine the looks of the graph
+plot2 <- immersiontime %>%
+  ggplot(aes(x = dpi, y = disease_percentage, color = trt, group = trt)) + #ggplot does the graph as multiple layers
+  stat_summary(geom = "point", position = "dodge") + #geom point plots every single data point
+  stat_summary(geom = "line", position = "dodge") + #stat_summary adds a layer with a summary statistic of the data, not the raw data
+  stat_summary(geom = "errorbar", position = "dodge") +
+  labs(x = "DPI", y = "% Chlorosis", title = "Immersion Time", color = "")
+
+print(plot2)
+
